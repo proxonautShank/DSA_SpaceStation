bbc7215ec755ad65f35d09eb3a342cf29ee5e05e
Shashank Pali <shashank.pali@aventri.com>
2022-07-07 13:31:41 +0530

    reference pattern

---
+++ Solution (1,2 & 3)/shank:anonymous.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+
+//MARK: - Challenge 1
+
+func isPalindrome(string: String) -> Bool {
+    // Solution
+    return true
+}
+
+//MARK: - Challenge 2
+
+func getLargestAdjacentProduct(items: [Int]) -> Int {
+    // Solution
+    return 1
+}
+
+//MARK: - Challenge 3
+
+func areaOfPolygon(side: Int) -> Int {
+    // Solution
+    return 1
+}
