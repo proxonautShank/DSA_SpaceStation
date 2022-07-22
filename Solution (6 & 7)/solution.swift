61f2394aef31d272ec0d0c4a95e176904fe19182
Shashank Pali <103515653+proxonautShank@users.noreply.github.com>
2022-07-22 13:57:05 +0530

    added solution

--- Solution (6 & 7)/solution.swift
+++ Solution (6 & 7)/solution.swift
@@ -6,9 +6,24 @@
 import Foundation
 
 //MARK: - Challenge 6
-
-func
+func arrange(anagram: [String]) -> [String] {
+    var ref : [(Int, String)] = []
+    for (i, str) in anagram.enumerated() {
+        ref += [(i, String(str.sorted()))]
+    }
+    ref = ref.sorted{$0.1 < $1.1}
+    
+    var res : [String] = []
+    
+    for tuple in ref {
+        res += [anagram[tuple.0]]
+    }
+    
+    return res
+}
 
 //MARK: - Challenge 7
-
-func
+func longestStrings(inputArray: [String]) -> [String] {
+    let max = inputArray.map{str in str.count}.max()
+    return inputArray.filter{ str in str.count == max }
+}
